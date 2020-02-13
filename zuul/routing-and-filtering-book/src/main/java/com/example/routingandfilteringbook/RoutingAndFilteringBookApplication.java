14fe5ff60bb8007cd3a95601f47332115648adb3
Siddhardha Nandipati <siddhardhan@gmail.com>
2020-02-12 22:32:52 -0500

    advanced swagger changes

--- zuul/routing-and-filtering-book/src/main/java/com/example/routingandfilteringbook/RoutingAndFilteringBookApplication.java
+++ zuul/routing-and-filtering-book/src/main/java/com/example/routingandfilteringbook/RoutingAndFilteringBookApplication.java
@@ -4,11 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class RoutingAndFilteringBookApplication {
 
