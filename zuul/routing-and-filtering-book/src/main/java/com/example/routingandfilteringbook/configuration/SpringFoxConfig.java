746fac3556d5edfc9f1c27496136ea4cc8cebec2
Siddhardha Nandipati <siddhardhan@gmail.com>
2020-02-12 22:42:08 -0500

    advanced swagger changes

--- zuul/routing-and-filtering-book/src/main/java/com/example/routingandfilteringbook/configuration/SpringFoxConfig.java
+++ zuul/routing-and-filtering-book/src/main/java/com/example/routingandfilteringbook/configuration/SpringFoxConfig.java
@@ -9,6 +9,7 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.*;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
@@ -32,6 +33,13 @@ public class SpringFoxConfig {
                 .license("Apache 2.0")
                 .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
                 .version("2.0.0")
+                .build();
+    }
+
+    @Bean
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .docExpansion(DocExpansion.LIST)
                 .build();
     }
 }
